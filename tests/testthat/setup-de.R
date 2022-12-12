cc003688aba7ed4eb0fec6516ae2130e803fdaf3
Thomas Sandmann <tomsing1@users.noreply.github.com>
2022-12-11 17:14:32 -0800

    Suggestions for the vignettes and the tests

--- tests/testthat/setup-de.R
+++ tests/testthat/setup-de.R
@@ -10,7 +10,8 @@ storage.mode(cnts) <- "integer"
 rownames(cnts) <- paste0("gene", seq_len(nrow(cnts)))
 cond <- factor(rep(1:2, each=5))
 
-se <- SummarizedExperiment(assays = list(counts = cnts), colData = DataFrame(cond))
+se <- SummarizedExperiment(assays = list(counts = cnts), 
+                           colData = DataFrame(cond))
 rm(cnts, cond)
 
 # DESeq2 ---
@@ -18,7 +19,7 @@ rm(cnts, cond)
 library(DESeq2)
 
 dds <- DESeqDataSet(se, ~ cond)
-dds <- DESeq(dds)
+dds <- DESeq(dds, fitType='local')
 res_deseq2 <- results(dds)
 
 rm(dds)
@@ -27,26 +28,23 @@ rm(dds)
 
 library(edgeR)
 
-counts <- assay(se, "counts")
 design <- model.matrix(~ 0 + cond, data = colData(se))
 
-fit <- glmFit(counts, design, dispersion=0.1)
+fit <- glmFit(se, design, dispersion=0.1)
 lrt <- glmLRT(fit, contrast = c(-1, 1))
 res_edger <- topTags(lrt, n = Inf)
 
-rm(counts, design, fit, lrt)
+rm(design, fit, lrt)
 
 # limma ----
 
-counts <- assay(se, "counts")
 design <- model.matrix(~ 0 + cond, data = colData(se))
 
-keep <- filterByExpr(counts, design)
-v <- voom(counts[keep,], design, plot=FALSE)
-fit <- lmFit(v, design)
-contr <- makeContrasts("cond2 - cond1", levels = colnames(coef(fit)))
-tmp <- contrasts.fit(fit, contr)
-tmp <- eBayes(tmp)
-res_limma <- topTable(tmp, sort.by = "P", n = Inf)
+keep <- filterByExpr(se, design)
+fit <- voomLmFit(se[keep,], design, plot = FALSE)
+contr <- makeContrasts("cond2 - cond1", levels = design)
+fit <- contrasts.fit(fit, contr)
+fit <- eBayes(fit)
+res_limma <- topTable(fit, sort.by = "P", n = Inf)
 
-rm(counts, design, keep, v, fit, contr, tmp)
+rm(design, keep, fit, contr)
