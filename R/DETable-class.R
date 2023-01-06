b1967f52293a0a8c2982116c7d70a742a401cc0d
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-01-06 11:27:28 +0000

    initialise a script for DETable

---
+++ R/DETable-class.R
@@ -0,0 +1,48 @@
+#' The DETable class
+#'
+#' The DETable is a \linkS4class{RowTable} subclass that is dedicated to creating a volcano plot.
+#' It retrieves the log-fold change and p-value from and creates a row-based plot where each point represents a feature.
+#'
+#' @docType methods
+#' @aliases DETable DETable-class
+#' initialize,DETable-method
+#' .fullName,DETable-method
+#' .panelColor,DETable-method
+#'
+#' @name DETable-class
+#'
+#' @examples
+#' x <- DETable()
+NULL
+
+#' @export
+#' @importClassesFrom iSEE RowTable
+setClass("DETable",
+    contains = "RowTable",
+    slots = c(ContrastName = "character")
+)
+
+#' @export
+#' @importMethodsFrom iSEE .fullName
+setMethod(".fullName", "DETable", function(x) "Differential expression table")
+
+#' @export
+#' @importMethodsFrom iSEE .panelColor
+setMethod(".panelColor", "DETable", function(x) "#DEAE10")
+
+
+#' @export
+#' @importMethodsFrom methods initialize
+#' @importFrom methods callNextMethod
+setMethod("initialize", "DETable", function(.Object,
+                                                ContrastName = NA_character_, ...) {
+    args <- list(ContrastName = ContrastName, ...)
+
+    do.call(callNextMethod, c(list(.Object), args))
+})
+
+#' @export
+#' @importFrom methods new
+DETable <- function(...) {
+    new("DETable", ...)
+}
