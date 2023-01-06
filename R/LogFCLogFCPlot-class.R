d02a0d56539869fc5eef4c1a45f6496677cefccd
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-01-06 16:09:10 +0000

    add dropdown to select contrast and observers

--- R/LogFCLogFCPlot-class.R
+++ R/LogFCLogFCPlot-class.R
@@ -6,9 +6,13 @@
 #' @docType methods
 #' @aliases LogFCLogFCPlot LogFCLogFCPlot-class
 #' initialize,LogFCLogFCPlot-method
+#' .cacheCommonInfo,LogFCLogFCPlot-method
+#' .createObservers,LogFCLogFCPlot-method
+#' .defineDataInterface,LogFCLogFCPlot-method
 #' .fullName,LogFCLogFCPlot-method
 #' .generateDotPlotData,LogFCLogFCPlot-method
 #' .panelColor,LogFCLogFCPlot-method
+#' .refineParameters,LogFCLogFCPlot-method
 #' 
 #' @name LogFCLogFCPlot-class
 #' 
@@ -61,6 +65,106 @@ LogFCLogFCPlot <- function(...) {
 #' @importFrom S4Vectors setValidity2
 setValidity2("LogFCLogFCPlot", function(object) {
   return(TRUE)
+})
+
+#' @export
+#' @importMethodsFrom iSEE .cacheCommonInfo
+#' @importFrom iSEE .getCachedCommonInfo .setCachedCommonInfo
+#' @importFrom methods callNextMethod
+#' @importFrom SummarizedExperiment rowData
+setMethod(".cacheCommonInfo", "LogFCLogFCPlot", function(x, se) {
+  if (!is.null(.getCachedCommonInfo(se, "LogFCLogFCPlot"))) {
+    return(se)
+  }
+  
+  se <- callNextMethod()
+  
+  contrast_names <- names(rowData(se)[["iSEEde"]])
+  
+  .setCachedCommonInfo(se, "LogFCLogFCPlot", valid.contrast.names = contrast_names)
+})
+
+#' @export
+#' @importMethodsFrom iSEE .refineParameters
+#' @importFrom iSEE .getCachedCommonInfo .replaceMissingWithFirst
+#' @importFrom methods callNextMethod
+setMethod(".refineParameters", "LogFCLogFCPlot", function(x, se) {
+  x <- callNextMethod() # Trigger warnings from base classes.
+  if (is.null(x)) {
+    return(NULL)
+  }
+  
+  contrast_names <- .getCachedCommonInfo(se, "LogFCLogFCPlot")$valid.contrast.names
+  x <- .replaceMissingWithFirst(x, .contrastNameX, contrast_names)
+  x <- .replaceMissingWithFirst(x, .contrastNameY, contrast_names)
+  
+  x
+})
+
+#' @export
+#' @importMethodsFrom iSEE .createObservers
+#' @importFrom iSEE .getEncodedName .createProtectedParameterObservers
+#' @importFrom methods callNextMethod
+setMethod(".createObservers", "LogFCLogFCPlot", function(x, se, input, session, pObjects, rObjects) {
+    callNextMethod()
+
+    plot_name <- .getEncodedName(x)
+
+    .createProtectedParameterObservers(plot_name,
+        fields=c(.contrastNameX, .contrastNameY),
+        input=input, pObjects=pObjects, rObjects=rObjects)
+    
+    invisible(NULL)
+})
+
+#' @export
+#' @importMethodsFrom iSEE .defineDataInterface
+#' @importFrom methods callNextMethod
+#' @importFrom shiny hr
+#' @importFrom iSEE .addSpecificTour .getCachedCommonInfo .getEncodedName
+#' .selectInput.iSEE
+setMethod(".defineDataInterface", "LogFCLogFCPlot", function(x, se, select_info) {
+  plot_name <- .getEncodedName(x)
+  input_FUN <- function(field) paste0(plot_name, "_", field)
+  # nocov start
+  .addSpecificTour(class(x), .contrastNameX, function(plot_name) {
+    data.frame(
+      rbind(
+        c(
+          element=paste0("#", plot_name, "_", sprintf("%s + .selectize-control", .contrastNameX)),
+          intro="Here, we select the name of the contrast to visualise on the x-axis amongst the choice of differential expression results available."
+        )
+      )
+    )
+  })
+  .addSpecificTour(class(x), .contrastNameY, function(plot_name) {
+    data.frame(
+      rbind(
+        c(
+          element=paste0("#", plot_name, "_", sprintf("%s + .selectize-control", .contrastNameY)),
+          intro="Here, we select the name of the contrast to visualise on the x-axis amongst the choice of differential expression results available."
+        )
+      )
+    )
+  })
+  # nocov end
+  cached <- .getCachedCommonInfo(se, "LogFCLogFCPlot")
+  
+  extra_inputs <- list(
+    .selectInput.iSEE(x, .contrastNameX, 
+                      label="Contrast (X):",
+                      selected=x[[.contrastNameX]],
+                      choices=cached$valid.contrast.names),
+    .selectInput.iSEE(x, .contrastNameX, 
+                      label="Contrast (Y):",
+                      selected=x[[.contrastNameX]],
+                      choices=cached$valid.contrast.names)
+  )
+  
+  c(callNextMethod(), 
+    list(hr()), 
+    extra_inputs
+  )
 })
 
 #' @export
