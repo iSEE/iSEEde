9e22207eb41cca9416f02db783642867cde0045b
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-01-06 12:07:40 +0000

    add dropdown to select contrast

--- R/DETable-class.R
+++ R/DETable-class.R
@@ -47,6 +47,66 @@ DETable <- function(...) {
     new("DETable", ...)
 }
 
+#' @importFrom S4Vectors setValidity2
+setValidity2("DETable", function(object) {
+    return(TRUE)
+})
+
+#' @export
+#' @importMethodsFrom iSEE .cacheCommonInfo
+#' @importFrom iSEE .getCachedCommonInfo .setCachedCommonInfo
+#' @importFrom methods callNextMethod
+#' @importFrom SummarizedExperiment rowData
+setMethod(".cacheCommonInfo", "DETable", function(x, se) {
+    if (!is.null(.getCachedCommonInfo(se, "DETable"))) {
+        return(se)
+    }
+
+    se <- callNextMethod()
+
+    contrast_names <- colnames(rowData(se)[["iSEEde"]])
+
+    .setCachedCommonInfo(se, "DETable", valid.contrast.names = contrast_names)
+})
+
+#' @export
+#' @importMethodsFrom iSEE .defineDataInterface
+#' @importFrom methods callNextMethod
+#' @importFrom shiny hr
+#' @importFrom iSEE .addSpecificTour .getCachedCommonInfo .getEncodedName
+#' .selectInput.iSEE
+setMethod(".defineDataInterface", "DETable", function(x, se, select_info) {
+    plot_name <- .getEncodedName(x)
+    input_FUN <- function(field) paste0(plot_name, "_", field)
+    # nocov start
+    .addSpecificTour(class(x), .contrastName, function(plot_name) {
+        data.frame(
+            rbind(
+                c(
+                    element = paste0("#", plot_name, "_", sprintf("%s + .selectize-control", .contrastName)),
+                    intro = "Here, we select the name of the contrast to visualise amongst the choice of differential expression results available."
+                )
+            )
+        )
+    })
+    # nocov end
+    cached <- .getCachedCommonInfo(se, "VolcanoPlot")
+
+    extra_inputs <- list(
+        .selectInput.iSEE(x, .contrastName,
+            label = "Contrast:",
+            selected = x[[.contrastName]],
+            choices = cached$valid.contrast.names
+        )
+    )
+
+    c(
+        callNextMethod(),
+        list(hr()),
+        extra_inputs
+    )
+})
+
 #' @export
 #' @importMethodsFrom iSEE .generateTable
 #' @importFrom SummarizedExperiment rowData
